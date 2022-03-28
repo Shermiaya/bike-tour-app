100f3b87a69cee56e006f8c5cfb7bd8fb66ac72d
Sraymond12 <47101154+Sraymond12@users.noreply.github.com>
2022-03-28 17:53:07 +0100

    Updated sign in tests

--- test/screens/authenticate/sign_in_form_test.dart
+++ test/screens/authenticate/sign_in_form_test.dart
@@ -4,9 +4,11 @@ import 'package:bike_tour_app/firebase_options.dart';
 import 'package:bike_tour_app/main.dart';
 import 'package:bike_tour_app/screens/authenticate/sign_in.dart';
 import 'package:bike_tour_app/screens/authenticate/sign_in_form.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:bike_tour_app/screens/authenticate/sign_in.dart';
 
 void main() {
   testWidgets('Testing sign in', (WidgetTester tester) async {
@@ -16,4 +18,5 @@ void main() {
     expect(x, findsOneWidget);
     assert(1==1);
     });
+
 }
--- test/screens/authenticate/sign_in_test.dart
+++ test/screens/authenticate/sign_in_test.dart
@@ -1,7 +1,5 @@
-import 'package:bike_tour_app/screens/authenticate/sign_in_form.dart';
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:bike_tour_app/screens/navigation/to_page.dart';
 import 'package:bike_tour_app/screens/wrapper.dart';
 import 'package:bike_tour_app/services/auth_service.dart';
 import 'package:flutter/material.dart';
@@ -12,8 +10,53 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
 
+
+import 'valid_regex.dart';
+void main() {
+  test('Empty email test', () {
+    var emptyInput = ' ';
+
+    var result = ValidRegex().getValidEmailRegExp().hasMatch(emptyInput);
+
+    expect(result, false);
   });
+
+  
+  test('Given empty password input', () async {
+    var emptyInput = ' ';
+
+    var result = ValidRegex().getValidPasswordRegExp().hasMatch(emptyInput);
+
+    expect(result, false);
+  });
+
+  
+  test('Given valid email address', () async {
+    var result = ValidRegex().getValidEmailRegExp().hasMatch('JohnDoe1@example.com');
+
+    expect(result, true);
+  });
+
+  
+  test('Given valid Password', () {  
+    var result = ValidRegex().getValidPasswordRegExp().hasMatch('Qwerty13!');
+
+    expect(result, true);
+  });
+
+  
+  test('Given Invalid Email', (){
+     var result = ValidRegex().getValidEmailRegExp().hasMatch('JohnDoe1example.com');
+
+    expect(result, false);
+  });
+
+  
+  test('Given Invalid Password', () {
+    var result = ValidRegex().getValidPasswordRegExp().hasMatch('happy');
+
+    expect(result, false);
+  });
+
 }
